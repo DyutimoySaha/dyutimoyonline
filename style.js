bbcd929f6ce124737391c1a8452aba9e33ed6952
Dyutimoy <mrinmoysaha64@gmail.com>
2023-01-23 21:37:30 +0530

    Pushing

---
+++ style.js
@@ -0,0 +1,39 @@
+let menu = document.querySelector('#menu-bars');
+let hero = document.querySelector('.hero');
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  hero.classList.toggle('active');
+}
+
+let themeToggler = document.querySelector('.theme-toggler');
+let toggleBtn = document.querySelector('.toggle-btn');
+
+toggleBtn.onclick = () =>{
+  themeToggler.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  hero.classList.remove('active');
+  themeToggler.classList.remove('active');
+}
+
+document.querySelectorAll('.theme-toggler .theme-btn').forEach(btn =>{
+  
+  btn.onclick = () =>{
+    let color = btn.style.background;
+    document.querySelector(':root').style.setProperty('--main-color', color);
+  }
+
+});
+document.getElementById("submit").addEventListener('click',(e)=>{
+  e.preventDefault();
+ var userid = "lbrM71-nZ-_Rs7oGC";
+ emailjs.init(userid);
+var email=document.getElementById('mail').value
+var contactDetail={
+  formEmail:email
+  }
+  emailjs.send('service_tcrfqi8','template_bi2r7od',contactDetail)
+  alert("sending email");
+})
